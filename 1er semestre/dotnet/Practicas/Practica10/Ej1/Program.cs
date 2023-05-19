6f1a05121c14f53c724a0c9c4d2e61963f5f2aa3
Pedro0604 <pedrospadari0604@gmail.com>
2023-05-19 16:13:30 -0300

    Practica 10 .net

---
+++ 1er semestre/dotnet/Practicas/Practica10/Ej1/Program.cs
@@ -0,0 +1,35 @@
+﻿var listaMultiplosDeCinco = Enumerable.Range(100, 101).Where(x => x % 5 == 0).ToList();
+var listaPrimosMenoresACien = Enumerable.Range(2, 10000000).Where(x =>
+{
+    bool esPrimo = true;
+    for (int i = 2; i <= Math.Sqrt(x); i++)
+    {
+        esPrimo = esPrimo && x % i != 0;
+    }
+    return esPrimo;
+}).ToList();
+var listaPotenciasDeDos = Enumerable.Range(0, 11).Select(x => Math.Pow(2, x)).ToList();
+var sumaPotenciasDeDos = listaPotenciasDeDos.Sum();
+var PromedioPotenciasDeDos = listaPotenciasDeDos.Average();
+var listaNCuadradoConUltimoDigito6 = Enumerable.Range(1, 20).Select(x => Math.Pow(x, 2)).Where(x => x % 10 == 6).ToList();
+var listaNombresDeLaSemanaConU = Enumerable.Range(0, 7).Select(x => (DayOfWeek)x).Where(x => x.ToString().Contains("u")).ToList();
+
+
+ImprimirLista(listaMultiplosDeCinco);
+ImprimirLista(listaPrimosMenoresACien);
+ImprimirLista(listaPotenciasDeDos);
+Console.WriteLine(sumaPotenciasDeDos);
+Console.WriteLine(PromedioPotenciasDeDos);
+ImprimirLista(listaNCuadradoConUltimoDigito6);
+ImprimirLista(listaNombresDeLaSemanaConU);
+
+void ImprimirLista<T>(List<T> l)
+{
+    string st = "";
+    foreach (T item in l)
+    {
+        st += item + " - ";
+    }
+    st = st.Remove(st.Length - 3);
+    Console.WriteLine(st);
+}
---
+++ 1er semestre/dotnet/Practicas/Practica10/Ej2/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] vector = new int[] { 1, 3, 4, 5, 9, 4, 3, 4, 5, 1, 1, 4, 9, 4, 3, 1 };
+vector.GroupBy(n => n)
+.ToList()
+.Select(g =>
+{
+    var obj = new
+    {
+        Numero = g.Key,
+        Cantidad = g.Count()
+    };
+    return obj;
+})
+.OrderBy(o => o.Cantidad)
+.ToList()
+.ForEach(obj => Console.WriteLine(obj));
