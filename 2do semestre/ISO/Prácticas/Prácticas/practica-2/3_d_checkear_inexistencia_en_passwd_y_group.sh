4bb90787e94fc3a085e28a9f59598574237f4bdc
Pedro0604 <pedrospadari0604@gmail.com>
2023-09-29 18:44:56 -0300

    ISO - Práctica2

--- 2do semestre/ISO/Prácticas/Prácticas/practica-2/3_d_checkear_inexistencia_en_passwd_y_group.sh
+++ 2do semestre/ISO/Prácticas/Prácticas/practica-2/3_d_checkear_inexistencia_en_passwd_y_group.sh
@@ -1,12 +1,12 @@
 name="$1"
 
-if [ $(cat /etc/passwd | cut -d":" -f1 | grep -w "$name" | wc -l) -eq 0 ];then
+if [ $(cat /etc/passwd | grep -w "$name" | wc -l) -eq 0 ]; then
 	echo "$name not found in /etc/passwd"
 else
 	echo "$name still exists in /etc/passwd"
 fi
 
-if [ $(cat /etc/group | cut -d":" -f4 | grep -w "$name" | wc -l ) -eq 0 ] && [ $(cat /etc/group | cut -d":" -f1 | grep -w "$name" | wc -l) -eq 0 ];then
+if [ $(cat /etc/group grep -w "$name" | wc -l) -eq 0 ]; then
 	echo "$name not found in /etc/group"
 else
 	echo "$name still exists in /etc/group"
