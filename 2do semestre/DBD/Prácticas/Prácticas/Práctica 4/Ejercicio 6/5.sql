da97fba967599584411c24e18f1054f5d4110aaa
Pedro0604 <pedrospadari0604@gmail.com>
2023-11-05 23:45:11 -0300

    DBD - Correcciones

--- 2do semestre/DBD/Prácticas/Prácticas/Práctica 4/Ejercicio 6/5.sql
+++ 2do semestre/DBD/Prácticas/Prácticas/Práctica 4/Ejercicio 6/5.sql
@@ -1,9 +1,22 @@
-SELECT r.nombre,
-    stock,
-    COUNT(DISTINCT re.codTec) as cantTecnicos
-FROM Repuesto r
-    LEFT JOIN Repuesto_Reparacion rr ON rr.codRep = r.codRep
-    INNER JOIN Reparacion re ON re.nroReparac = rr.nroReparac
-GROUP BY r.codRep,
-    r.nombre,
-    stock
+(
+    SELECT r.nombre,
+        stock,
+        COUNT(DISTINCT re.codTec) as cantTecnicos
+    FROM Repuesto r
+        INNER JOIN Repuesto_Reparacion rr ON rr.codRep = r.codRep
+        INNER JOIN Reparacion re ON re.nroReparac = rr.nroReparac
+    GROUP BY r.codRep,
+        r.nombre,
+        stock
+)
+UNION
+(
+    SELECT r.nombre,
+        stock,
+        0 as cantTecnicos
+    FROM Repuesto r
+    WHERE r.codRep NOT IN(
+            SELECT rr.codRep
+            FROM Repuesto_Reparacion
+        )
+)
